e90bf2ff5ebfb9c1c78cc988499df64c5b1792ca
Gustavo Yuri Nishi <gustavoyurinishi@gmail.com>
2025-05-02 02:07:57 -0300

    update

--- witch's quest/game.js
+++ witch's quest/game.js
@@ -5,148 +5,103 @@ let ctx = canvas.getContext('2d');
 
 let player = {
     sx: 0,
-    sy: 1,
-    x: 375,
-    y: 250,
-    size: 128,
+    sy: 0,
+    x: 100,
+    y: 600,
+    size: 100,
     img: new Image(),
     desenha: function(){
-        this.img.src = "player_spritesheet.png";
+        this.img.src = "witch_spritesheet.png";
         ctx.beginPath();
-        ctx.drawImage(this.img, 0 + (256*this.sx), 0 + (256*this.sy), 256, 256,  this.x, this.y, this.size, this.size);
-        ctx.closePath();
-    }
-}
-
-let imagem_menu = {
-    local: 0,
-    size: 800,
-    img: new Image(),
-    desenha2: function(){
-        this.img.src = 'menu.png';
-        ctx.beginPath();
-        ctx.drawImage(this.img, 0, 0, 800, 800, this.local, this.local, this.size, this.size);
+        ctx.drawImage(this.img, 0 + (100*this.sx), 0 + (100*this.sy), 100, 100,  this.x, this.y, this.size, this.size);
         ctx.closePath();
     }
 }
 
 
 function animacao(){
-    ctx.clearRect(0,0,900,900);
+    ctx.clearRect(0,0,1200,800);
     player.desenha();
     requestAnimationFrame(animacao);
 }
 
 animacao()
 
-battle = 0
-menu = 0
 frame = 0
 if_frame = 10
-player.sx = 0
+player.sx = 1
+andar_direita = false
+andar_esquerda = false
+correr = false
 
 document.addEventListener('keydown', function(evento){
     let tecla = evento.key;
     console.log(tecla);
 
     let velocidade = 6;
-    if(tecla == 'ArrowUp'){
-        player.y -= velocidade;
+
+    if(tecla == 'Shift'){ 
+        correr = true
+    }
+    if(correr == true){
+        velocidade = 12
+        if_frame = 5
+    }
+
+    if(tecla == 'ArrowRight'){
+        andar_direita = true
+    }
+    if(andar_direita == true){ 
+        player.x += velocidade;
         player.sx = player.sx;
         player.sy = 0;
         frame += 1;
-        if(frame >= if_frame && player.sx == 0){
+        if(frame >= if_frame && player.sx == 2){
             player.sx = 1;
             frame = 0;
         }
         if(frame >= if_frame && player.sx == 1){
-            player.sx = 0;
+            player.sx = 2;
             frame = 0;
         }
     }
-    if(tecla == 'ArrowDown'){
-        player.y += velocidade;
+
+    if(tecla == 'ArrowLeft'){
+        andar_esquerda = true
+    }
+    if(andar_esquerda == true){
+        player.x -= velocidade;
         player.sx = player.sx;
         player.sy = 1;
         frame += 1;
-        if(frame >= if_frame && player.sx == 0){
+        if(frame >= if_frame && player.sx == 2){
             player.sx = 1;
             frame = 0;
         }
         if(frame >= if_frame && player.sx == 1){
-            player.sx = 0;
+            player.sx = 2;
             frame = 0;
         }
     }
-    if(tecla == 'ArrowLeft'){
-        player.x -= velocidade;
-        player.sx = player.sx;
-        player.sy = 2;
-        frame += 1;
-        if(frame >= if_frame && player.sx == 0){
-            player.sx = 1;
-            frame = 0;
-        }
-        if(frame >= if_frame && player.sx == 1){
-            player.sx = 0;
-            frame = 0;
-        }
-    }
-    if(tecla == 'ArrowRight'){ 
-        player.x += velocidade;
-        player.sx = player.sx;
-        player.sy = 3;
-        frame += 1;
-        if(frame >= if_frame && player.sx == 0){
-            player.sx = 1;
-            frame = 0;
-        }
-        if(frame >= if_frame && player.sx == 1){
-            player.sx = 0;
-            frame = 0;
-        }
-    }
-        //DEBUG
-    if(tecla == 'KeyD'){
-        battle = 1
-    }
-        //
-    if(67 in tecla){
-        menu = 1
-    }   
 })
 
+document.addEventListener('keyup', function(evento){
+    let tecla_solta = evento.key;
+    console.log(tecla_solta);
 
-//////PLAYER STATS
-
-level = 1
-exp = 0
-player_maxhp = 20
-player_currenthp = player_maxhp 
-player_maxmp = 30
-player_currentmp = player_maxmp
-player_attack = 6
-player_defense = 5
-player_magic = 12
-player_speed = 10
-
-//////ENEMY STATS
-
-class enemy {
-    constructor(enemy_id, enemy_name, enemy_maxhp, enemy_attack, enemy_defense, enemy_speed, enemy_exp){
-        this.enemy_id = enemy_id
-        this.enemy_name = enemy_name
-        this.enemy_maxhp = enemy_maxhp
-        this.enemy_attack = enemy_attack
-        this.enemy_defense = enemy_defense
-        this.enemy_speed = enemy_speed
-        this.enemy_exp = enemy_exp
+    if(tecla_solta == 'ArrowRight'){
+        andar_direita = false
+        player.sx = 1
     }
-}
 
+    if(tecla_solta == 'ArrowLeft'){
+        andar_esquerda = false
+        player.sx = 1
+    }
 
-//////
-
-if(menu == 1){
-    imagem_menu.desenha2()
-}
+    if(tecla_solta == 'Shift'){
+        correr = false
+        velocidade = 6
+        if_frame = 10
+    }
+})
