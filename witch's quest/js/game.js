dbb28f132a68049c283a9d4cde837cb19fe5beb3
Gustavo Yuri Nishi <gustavoyurinishi@gmail.com>
2025-05-13 16:16:42 -0300

    updatre

--- witch's quest/js/game.js
+++ witch's quest/js/game.js
@@ -1,28 +1,15 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext('2d');
 
-let map_test = {
-    x: 0,
-    y: 0,
-    tamanho: 800,
-    img: new Image(),
-    desenha: function(){
-        this.img.src = '../dataset/map_test.png';
-        ctx.beginPath();
-        ctx.drawImage(this.img, this.x, this.y, this.tamanho*2, this.tamanho);
-        ctx.closePath();
-    }
-}
-
 let player = {
-    sx: 0,
+    sx: 1,
     sy: 0,
-    x: 100,
-    y: 700,
-    size: 150,
+    x: 400,
+    y: 400,
+    size: 100,
     img: new Image(),
     desenha: function(){
-        this.img.src = "../dataset/witch_spritesheet.png";
+        this.img.src = "../dataset/player_sprite.png";
         ctx.beginPath();
         ctx.drawImage(this.img, this.size * this.sx, this.size * this.sy, this.size, this.size, this.x, this.y, this.size, this.size);
         ctx.closePath();
@@ -37,10 +24,7 @@ let player = {
 //    baixo: player.y + player.size
 //}
 
-// se sy = 0, o personagem esta virado para direita
-// se sy = 1, o personagem esta virado para esquerda
-
-
+let velocidade = 2
 let frame = 0;
 let if_frame = 24;
 let andar_direita = false;
@@ -48,6 +32,7 @@ let andar_esquerda = false;
 let andar_cima = false;
 let andar_baixo = false;
 let correr = false;
+let animacao_vertical = 0
 
 player.sx = 1;
 
@@ -98,11 +83,13 @@ document.addEventListener('keyup', function(evento){
     if(tecla_solta === 'ArrowUp'){
         andar_cima = false;
         player.sx = 1;
+        animacao_vertical = 0
     }
 
     if(tecla_solta === 'ArrowDown'){
         andar_baixo = false;
         player.sx = 1;
+        animacao_vertical = 0
     }
 
     if(tecla_solta === 'Shift'){
@@ -118,65 +105,77 @@ function animacao(){
     player.desenha();
 
     // Movimento
-    if(andar_direita){
+    if(andar_direita == true){
         player.x += velocidade;
-        player.sy = 0;
-        frame++;
-        if(frame >= if_frame){
-            player.sx = (player.sx === 1) ? 2 : 1;
-            frame = 0;
+        player.sy = 2;
+        frame += 1
+        if(frame >= if_frame && player.sx == 1){
+            player.sx = 2
+            frame = 0
+        }
+        if(frame >= if_frame && player.sx == 2){
+            player.sx = 1
+            frame = 0
         }
     }
-    if(andar_esquerda){
+    if(andar_esquerda == true){
         player.x -= velocidade;
         player.sy = 1;
-        frame++;
-        if(frame >= if_frame){
-            player.sx = (player.sx === 1) ? 2 : 1;
-            frame = 0;
+        frame += 1
+        if(frame >= if_frame && player.sx == 1){
+            player.sx = 2
+            frame = 0
         }
-    }if(andar_cima){
-        player.x -= velocidade;
+        if(frame >= if_frame && player.sx == 2){
+            player.sx = 1
+            frame = 0
+        }
+    }
+    if(andar_cima == true){
+        player.y -= velocidade;
+        player.sy = 3;
+        frame += 1
+        if(frame >= if_frame && player.sx == 1 && animacao_vertical == 0){
+            player.sx = 2
+            frame = 0
+        }
+        if(frame >= if_frame && player.sx == 2 && animacao_vertical == 0){
+            player.sx = 1
+            frame = 0
+            animacao_vertical = 1
+        }
+        if(frame >= if_frame && player.sx == 1 && animacao_vertical == 1){
+            player.sx = 3
+            frame = 0
+        }
+        if(frame >= if_frame && player.sx == 3 && animacao_vertical == 1){
+            player.sx = 1
+            frame = 0
+            animacao_vertical = 0
+        }
+    }
+    if(andar_baixo == true){
+        player.y += velocidade;
         player.sy = 0;
-        frame++;
-        if(frame >= if_frame){
-            player.sx = (player.sx === 1) ? 2 : 1;
-            frame = 0;
+        frame += 1
+        if(frame >= if_frame && player.sx == 1 && animacao_vertical == 0){
+            player.sx = 2
+            frame = 0
         }
-    }
-    if(andar_baixo){
-        player.x += velocidade;
-        player.sy = 1;
-        frame++;
-        if(frame >= if_frame){
-            player.sx = (player.sx === 1) ? 2 : 1;
-            frame = 0;
+        if(frame >= if_frame && player.sx == 2 && animacao_vertical == 0){
+            player.sx = 1
+            frame = 0
+            animacao_vertical = 1
         }
-    }
-
-    // Gravidade (cair)
-    if(player.y + player.size < CHAO_Y){
-        cair = true;
-        onground = false;
-    } else {
-        cair = false;
-        onground = true;
-        pulos_realizados = 0; // reset double jump
-        player.y = CHAO_Y - player.size; // manter alinhado ao chão
-    }
-
-    if(cair && !pular){
-        player.y += gravidade;
-        player.sx = 3;
-    }
-
-    //teste
-    if(player.x + 50 <= 0){
-        player.x = -50
-    }
-
-    if(player.x - 50 >= 1450){
-        player.x = 1500
+        if(frame >= if_frame && player.sx == 1 && animacao_vertical == 1){
+            player.sx = 3
+            frame = 0
+        }
+        if(frame >= if_frame && player.sx == 3 && animacao_vertical == 1){
+            player.sx = 1
+            frame = 0
+            animacao_vertical = 0
+        }
     }
 
     requestAnimationFrame(animacao);
