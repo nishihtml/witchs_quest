73d56c96e388d6cd6c8abc76232f263cfbcb67b6
Gustavo Yuri Nishi <gustavoyurinishi@gmail.com>
2025-05-19 02:01:47 -0300

    update

--- witch's quest/js/game.js
+++ witch's quest/js/game.js
@@ -3,7 +3,7 @@ let ctx = canvas.getContext('2d');
 
 let inicio = true
 let jogo = false
-let gameover = false
+let fim = false
 
 let tutorial = {
     sx: 0,
@@ -65,12 +65,35 @@ let enemy = {
     }
 }
 
-// hitbox do player
-//    esquerda: player.x + 50,
-//    direita: player.x + player.size - 50,
-//    cima: player.y + 16,
-//    baixo: player.y + player.size
-//}
+let magia = {
+    sx: 0,
+    sy: 0,
+    x: 0,
+    y: 0,
+    size: 40,
+    img: new Image(),
+    desenha: function(){
+        this.img.src = "../dataset/bullet_sprite.png";
+        ctx.beginPath();
+        ctx.drawImage(this.img, this.size * this.sx, this.size * this.sy, this.size, this.size, this.x, this.y, this.size, this.size);
+        ctx.closePath();
+    }
+}
+
+let gameover = {
+    sx: 0,
+    sy: 0,
+    x: 0,
+    y: 0,
+    size: 800,
+    img: new Image(),
+    desenha: function(){
+        this.img.src = "../dataset/gameover.png";
+        ctx.beginPath();
+        ctx.drawImage(this.img, this.size * this.sx, this.size * this.sy, this.size, this.size, this.x, this.y, this.size, this.size);
+        ctx.closePath();
+    }
+}
 
 let velocidade = 2
 let frame = 0;
@@ -95,6 +118,10 @@ let tempo_empurro = 0
 let invencibilidade = false
 let tempo_invencibilidade = 0
 let player_parado = false
+let frame_dano = 0.5
+
+let atacar = false
+let bullet = false
 
 player.sx = 1;
 
@@ -125,6 +152,13 @@ document.addEventListener('keydown', function(evento){
     if(tecla === 'ArrowDown'){
         andar_cima = false;
         andar_baixo = true
+    }
+    if(tecla === ' '){
+        atacar = true
+        andar_direita = false;
+        andar_esquerda = false;
+        andar_cima = false;
+        andar_baixo = false
     }
 
 });
@@ -194,6 +228,39 @@ function animacao(){
     }
     //---FIM DO TUTORIAL
 
+    //INICIO DO GAMEOVER
+    if(fim == true){
+        gameover.desenha()
+        botao.desenha()
+        document.addEventListener('mousemove', function(evento){
+            let rect = canvas.getBoundingClientRect()
+            let x_mouse = evento.clientX - rect.left
+            let y_mouse = evento.clientY - rect.top;
+
+            if(x_mouse >= 540 && x_mouse <= 740 && y_mouse >= 670 && y_mouse <= 750){
+                botao.sx = 1
+            }
+            else{
+                botao.sx = 0
+            }
+        })
+        canvas.addEventListener('click', (evento) => {
+            let rect = canvas.getBoundingClientRect()
+            let x_mouse = evento.clientX - rect.left;
+            let y_mouse = evento.clientY - rect.top;
+
+            if(x_mouse >= 540 && x_mouse <= 740 && y_mouse >= 670 && y_mouse <= 750){
+                vida_player = 3
+                player.x = 400
+                player.y = 400
+                fim = false
+                jogo = true
+            }
+        });
+    }
+    //FIM DO GAMEOVER
+
+
     //INICIO DO JOGO---
     if(jogo == true){
         //PLAYER
@@ -201,7 +268,7 @@ function animacao(){
             frame += 1
         }
         player.desenha();
-        if(andar_direita == true && player_parado == false){
+        if(andar_direita == true && player_parado == false && atacar == false){
             player.x += velocidade;
             player.sy = 2;
             if(frame >= if_frame && player.sx == 1 && animacao_vertical == 0){
@@ -293,20 +360,20 @@ function animacao(){
         //ENEMY
 
         enemy.desenha()
-        if(player.x > enemy.x){
-            enemy.x += enemy_velocidade
-            enemy.sy = 1 
-        }
-        if(player.x < enemy.x){
-            enemy.x -= enemy_velocidade
-            enemy.sy = 0
-        }
-        if(player.y > enemy.y){
-            enemy.y += enemy_velocidade
-        }
-        if(player.y < enemy.y){
-            enemy.y -= enemy_velocidade
-        }
+        // if(player.x > enemy.x){
+        //     enemy.x += enemy_velocidade
+        //     enemy.sy = 1 
+        // }
+        // if(player.x < enemy.x){
+        //     enemy.x -= enemy_velocidade
+        //     enemy.sy = 0
+        // }
+        // if(player.y > enemy.y){
+        //     enemy.y += enemy_velocidade
+        // }
+        // if(player.y < enemy.y){
+        //     enemy.y -= enemy_velocidade
+        // }
         enemy_frame += 1
         if(enemy_if_frame <= enemy_frame && enemy.sx == 1){
             enemy.sx = 2
@@ -318,11 +385,12 @@ function animacao(){
         }
 
         //COLLISÃO ENTRE PLAYER E ENEMY
-
-        if(player.y <= enemy.y + 100 && player.y + 100 >= enemy.y && player.x <= enemy.x + 100 && player.x + 100 >= enemy.x && invencibilidade == false){
+        //PLAYER CIMA X ENEMY BAIXO          PLAYER BAIXO X ENEMY CIMA         PLAYER ESQUERDA X ENEMY DIREITA        PALYER DIREITA X ENEMY ESQUERDA
+        if(player.y + 20 <= enemy.y + 100 && player.y + 100 >= enemy.y + 20 && player.x + 20 <= enemy.x - 20 + 100 && player.x - 20 + 100 >= enemy.x + 20 && invencibilidade == false){
             direcao_empurro = Math.floor(Math.random() * 3);
             empurrado = true
             invencibilidade = true
+            vida_player -= 1
         }
         if(empurrado == true){
             andar_cima = false
@@ -359,22 +427,102 @@ function animacao(){
             andar_direita = false
             andar_esquerda = false
             tempo_empurro += 1
-            if(tempo_empurro == 10){
+            if(tempo_empurro >= 10){
                 player_parado = false
                 tempo_empurro = 0
             }
         }
         if(invencibilidade == true){
             tempo_invencibilidade += 1
-            if(tempo_invencibilidade >= 1000){
+            frame_dano += 1
+            if(tempo_invencibilidade >= 300){
                 invencibilidade = false
                 tempo_invencibilidade = 0
+                player.sx = 1
             }
+            if(frame_dano >= if_frame /8 && player.sx !== 0){
+                player.sx = 0
+                frame_dano = 0
+            }
+            if(frame_dano >= if_frame /8 && player.sx == 0){
+                player.sx = 1
+                frame_dano = 0
+            }
+        }
+
+        //ATAQUE DO PLAYER
+        // if(atacar == true && player_parado == false && empurrado == false && bullet == false){
+        //     player.sx = 4
+        //     andar_cima = false
+        //     andar_baixo = false
+        //     andar_direita = false
+        //     andar_esquerda = false
+        //     if(player.sy == 0){
+        //         andar_cima = false
+        //         andar_direita = false
+        //         andar_esquerda = false
+        //         magia.sx = 0
+        //         magia.sy = 0
+        //         magia.desenha()
+        //         bullet == true
+        //         magia.y += velocidade * 2.5
+        //     }
+        //     if(player.sy == 1){
+        //         andar_baixo = false
+        //         andar_cima = false
+        //         andar_esquerda = false
+        //         magia.sx = 0
+        //         magia.sy = 1
+        //         magia.desenha()
+        //         bullet == true
+        //         magia.x -= velocidade * 2.5
+        //     }
+        //     if(player.sy == 2){
+        //         andar_baixo = false
+        //         andar_cima = false
+        //         andar_direita = false
+        //         magia.sx = 1
+        //         magia.sy = 1
+        //         magia.desenha()
+        //         bullet == true
+        //         magia.x += velocidade * 2.5
+        //     }
+        //     if(player.sy == 3){
+        //         andar_cima = false
+        //         andar_direita = false
+        //         andar_esquerda = false
+        //         magia.sx = 1
+        //         magia.sy = 0
+        //         magia.desenha()
+        //         bullet == true
+        //         magia.y -= velocidade * 2.5
+        //     }
+        // }
+
+        //COLLISÃO DE PAREDE
+        if(player.x <= 0){
+            player.x = 0
+        }
+        if(player.x >= 700){
+            player.x = 700
+        }
+        if(player.y <= 0){
+            player.y = 0
+        }
+        if(player.y >= 700){
+            player.y = 700
+        }
+
+        //CONDIÇÃO DE FIM DE JOGO
+        if(vida_player <= 0){
+            jogo = false
+            fim = true
         }
         
     //FIM DO JOGO
 
+    
     }    
-requestAnimationFrame(animacao);
+    requestAnimationFrame(animacao);
 }
 animacao()
