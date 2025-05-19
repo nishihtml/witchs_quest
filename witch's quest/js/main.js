525294f74319e762646cab599fd17e1f743738d2
SunizePlatform <contato.sunize@gmail.com>
2025-05-19 11:10:08 -0300

    Updates

--- witch's quest/js/main.js
+++ witch's quest/js/main.js
@@ -1,29 +1,6 @@
-function startGame() {
-    const preview = document.getElementById('game-preview');
-    const text = preview.querySelector('.game-text');
-    const loader = preview.querySelector('.loader');
-
-    preview.classList.remove('hidden');
-    loader.classList.remove('hidden');
-
-    let dots = 0;
-
-    const interval = setInterval(() => {
-        dots = (dots + 1) % 4;
-        text.textContent = 'Carregando magia' + '.'.repeat(dots);
-    }, 500);
-
+window.addEventListener('load', () => {
+    const loadingScreen = document.getElementById('loading-screen');
     setTimeout(() => {
-        clearInterval(interval);
-        text.textContent = 'Magia carregada! Vamos jogar!';
-        text.style.color = '#c084fc';
-
-        // Oculta loader
-        loader.classList.add('hidden');
-
-        // Esconde o preview inteiro após 1.5s
-        setTimeout(() => {
-            preview.classList.add('hidden');
-        }, 1500);
-    }, 3500);
-}
+        loadingScreen.style.display = 'none';
+    }, 1000); // Dura 2 segundos
+});
